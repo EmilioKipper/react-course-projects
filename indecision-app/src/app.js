34149562c23183bb3c7c330c1f09837f195e913f
Emilio Gularte Kipper <emilio.kipper@rede.ulbra.br>
2019-06-05 09:51:30 -0300

    .

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -27,19 +27,29 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert('handlePick')
+  }
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
+
+
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert('something');
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         {
           this.props.options.map((option) => <Option key={option} optionText={option} />)
         }
@@ -59,13 +69,25 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
-  render() {
-    return (
-      <div>
-        AddOption component here
-      </div>
-    );
+    handleAddOption(e) {
+      e.preventDefault();
+
+      const option = e.target.elements.option.value;
+
+      if(option) {
+        alert(option);
+      }
+    }
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleAddOption}>
+            <input type="text" name="option" />
+            <button>Add Option</button>
+          </form>
+        </div>
+      );
+    }
   }
-}
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
